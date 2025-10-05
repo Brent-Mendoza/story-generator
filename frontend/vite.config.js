fc07120229ec1af9e1d97a306be4691fcad518dd
Brent-Mendoza <mjustinebrent@gmail.com>
2025-10-05 21:05:57 +0800

    vite config fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
       "/api": {
         target: process.env.VITE_BACKEND_URL,
         changeOrigin: true,
-        secure: false,
+        secure: process.env.VITE_NODE_ENV === "production",
       },
     },
   },
